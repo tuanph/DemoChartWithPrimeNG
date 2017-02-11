698e69517e0f1ab4312b13b178e50419d272c808
DESKTOP-7QMDHSD\TUANPH-LAPTOP <tuan.phamhoang.2506@gmail.com>
2017-02-11 10:18:25 +0700

    Create the first Chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,46 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    //   template: `<p-chart #chart type="pie" [data]="data"></p-chart>
+    // <button type="button" pButton (click)="update(chart)"></p-button>`,
+    template: `
+    <h1>My First PrimeNG App</h1>
+   <button pButton type="button" label="Click"></button>
+   <p-chart type="bar" [data]="data" [options]="options"  ></p-chart>
+`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+    data: any;
+    options: any;
+    constructor() {
+         this.data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'My First dataset',
+                    backgroundColor: '#42A5F5',
+                    borderColor: '#1E88E5',
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: 'My Second dataset',
+                    backgroundColor: '#9CCC65',
+                    borderColor: '#7CB342',
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        }
+        this.options = {
+            title: {
+                display: true,
+                text: 'My Title',
+                fontSize: 16
+            },
+            legend: {
+                position: 'bottom'
+            }
+        };
+
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {ChartModule} from 'primeng/primeng';
+import {InputTextModule} from 'primeng/primeng';
+import {ButtonModule} from 'primeng/primeng';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule ,ChartModule,InputTextModule,ButtonModule],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
